9fa53ad67da1f3af7ffa95e17a96e2cf7473ed3c
Demira11 <117664846+Demira11@users.noreply.github.com>
2022-11-14 10:10:06 -0500

    Added axios and apiKey-400 error

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+function displayTemperature(reposnse) {
+  console.log(response.data);
+}
+
+let apiKey = "19a7287a43046ce253c65a1908dfe8b1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
