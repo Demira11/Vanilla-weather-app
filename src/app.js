9027bcef9353371ffff9ba0f2b69924d2397739a
Demira11 <117664846+Demira11@users.noreply.github.com>
2022-12-01 09:20:29 -0500

    updated api key

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "3b1546503c8ddc7f105a8d1fe0d4b554";
+  let apiKey = "17ad6e67aa629189f73b053634668b20";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiURL).then(displayForecast);
@@ -101,7 +101,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "19a7287a43046ce253c65a1908dfe8b1";
+  let apiKey = "17ad6e67aa629189f73b053634668b20";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
